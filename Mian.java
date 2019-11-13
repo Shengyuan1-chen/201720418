b7726c4c45ddf2b2df14edee4829b457670a0916
a975381975 <57627401+a975381975@users.noreply.github.com>
2019-11-14 00:35:38 +0800

    Add files via upload

---
+++ Mian.java
@@ -0,0 +1,14 @@
+package work;
+
+public class Mian {
+
+	public static void main(String[] args) {
+		//实例化一个Exercise类的对象
+		Exercise exercise = new Exercise();
+		
+		//格式化输出
+		exercise.Formatted();
+
+	}
+
+}
